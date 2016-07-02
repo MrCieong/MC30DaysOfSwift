bb805e2f6aca2e762ff2bf27601f0149a4f9ca46
MrCieong <zhangjing0829@gmail.com>
2016-07-02 17:42:57 +0800

    Add CustomFont
    Add CustomFont

---
+++ CustomFont/CustomFont/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  CustomFont
+//
+//  Created by zhangjing on 16/7/2.
+//  Copyright © 2016年 MrCieong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  @IBOutlet weak var tableView: UITableView!
+  let data = ["30 Days Swift", "这些字体特别适合打「奋斗」和「理想」", "谢谢「造字工房」，本案例不涉及商业使用", "使用到造字工房劲黑体，致黑体，童心体", "呵呵，再见🤗 See you next Project", "Mr Cieong"]
+  
+  let fontNames = ["MFTongXin_Noncommercial-Regular", "MFJinHei_Noncommercial-Regular", "MFZhiHei_Noncommercial-Regular"]
+  var currentFontIndex = 0
+  
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+  }
+
+  @IBAction func changeFont(sender: AnyObject) {
+    currentFontIndex = (currentFontIndex + 1) % 3
+    tableView.reloadData()
+  }
+}
+
+extension ViewController: UITableViewDataSource {
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return data.count
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("FontCell")!
+    cell.textLabel?.text = data[indexPath.row]
+    cell.textLabel?.font = UIFont(name: fontNames[currentFontIndex], size: 32)
+    return cell
+  }
+}
+
