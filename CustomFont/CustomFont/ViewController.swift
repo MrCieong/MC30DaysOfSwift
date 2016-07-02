9876aca8499c1497ab2e5b8360de2c91617dab2a
MrCieong <zhangjing0829@gmail.com>
2016-07-02 19:12:52 +0800

    Add README file
    Add README file

--- CustomFont/CustomFont/ViewController.swift
+++ CustomFont/CustomFont/ViewController.swift
@@ -14,17 +14,25 @@ class ViewController: UIViewController {
   let data = ["30 Days Swift", "这些字体特别适合打「奋斗」和「理想」", "谢谢「造字工房」，本案例不涉及商业使用", "使用到造字工房劲黑体，致黑体，童心体", "呵呵，再见🤗 See you next Project", "Mr Cieong"]
   
   let fontNames = ["MFTongXin_Noncommercial-Regular", "MFJinHei_Noncommercial-Regular", "MFZhiHei_Noncommercial-Regular"]
-  var currentFontIndex = 0
+  var currentFontIndex = 0 {
+    didSet {
+      tableView.reloadData()
+    }
+  }
   
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    
+    for familyName in UIFont.familyNames() {
+      for fontName in UIFont.fontNamesForFamilyName(familyName) {
+        print(fontName)
+      }
+    }
   }
 
   @IBAction func changeFont(sender: AnyObject) {
     currentFontIndex = (currentFontIndex + 1) % 3
-    tableView.reloadData()
+    
   }
 }
 
---
+++ README.md
@@ -0,0 +1,6 @@
+## 30天学习Swift
+
+参考了allenwong的30DaysofSwift：[博文](http://weibo.com/ttarticle/p/show?id=2309403942494873235448)
+
+### 自定义字体
+![](https://github.com/MrCieong/MC30DaysOfSwift/gif/ChangFont.gif)
