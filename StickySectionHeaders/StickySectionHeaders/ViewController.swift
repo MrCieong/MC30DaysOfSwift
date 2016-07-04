d98433175c70c7bab9db689dd1941e65fc42ff08
MrCieong <zhangjing0829@gmail.com>
2016-07-04 23:16:00 +0800

    Add StickySectionHeaders
    Add StickySectionHeaders

---
+++ StickySectionHeaders/StickySectionHeaders/ViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ViewController.swift
+//  StickySectionHeaders
+//
+//  Created by zhangjing on 16/7/4.
+//  Copyright © 2016年 MrCieong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
+extension ViewController {
+  override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 10
+  }
+  
+  override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    return 20
+  }
+  
+  override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+    return cell
+  }
+}
